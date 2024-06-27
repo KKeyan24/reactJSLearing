acd56019b15b2731f317f13eb20514476317a027
KKeyan24 <karthite94@gmail.com>
2024-06-27 11:31:13 +0530

    RJS:- Fragemnt and Pure Component

---
+++ src/Other/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class FragmentDemo extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Demo</h1>
+        <p>Here is the Demo</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default FragmentDemo
